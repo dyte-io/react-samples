8a15a4e63d4d7a2023883d1a173eae72cdab8379
Vaibhav Shinde <vaibhav@dyte.io>
2023-06-06 00:09:04 +0530

    feat: added clubhouse sample

---
+++ samples/clubhouse/src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import { DyteProvider, useDyteClient } from '@dytesdk/react-web-core';
+import Meeting from './components/Meeting';
+
+function App() {
+  const [meeting, initMeeting] = useDyteClient();
+
+  useEffect(() => {
+    const searchParams = new URL(window.location.href).searchParams;
+
+    const authToken = searchParams.get('authToken');
+
+    // pass an empty string when using v2 meetings
+    // for v1 meetings, you would need to pass the correct roomName here
+    const roomName = searchParams.get('roomName') || '';
+
+    if (!authToken) {
+      alert(
+        "An authToken wasn't passed, please pass an authToken in the URL query to join a meeting."
+      );
+      return;
+    }
+
+    initMeeting({
+      authToken,
+      roomName,
+      // apiBase: 'https://api.staging.dyte.in',
+      defaults: {
+        video: false,
+        audio: false,
+      },
+    }).then((m) => {
+      Object.assign(window, { meeting: m });
+      m?.joinRoom();
+    });
+  }, []);
+
+  return (
+    <div className="w-full max-w-lg mx-auto h-full bg-gray-100">
+      <DyteProvider value={meeting} fallback={<div>loading...</div>}>
+        <Meeting />
+      </DyteProvider>
+    </div>
+  );
+}
+
+export default App;
