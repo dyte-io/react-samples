408981bde405306b16d9c611811b587843821cb5
Palash <pgolecha12@gmail.com>
2023-09-21 17:27:43 +0530

    fix: remove v1 roomName parameter

--- samples/clubhouse/src/App.tsx
+++ samples/clubhouse/src/App.tsx
@@ -15,10 +15,6 @@ function App() {
 
     const authToken = searchParams.get('authToken');
 
-    // pass an empty string when using v2 meetings
-    // for v1 meetings, you would need to pass the correct roomName here
-    const roomName = searchParams.get('roomName') || '';
-
     if (!authToken) {
       alert(
         "An authToken wasn't passed, please pass an authToken in the URL query to join a meeting."
@@ -28,7 +24,6 @@ function App() {
 
     initMeeting({
       authToken,
-      roomName,
       defaults: {
         video: false,
         audio: false,
--- samples/simple-group-call/src/App.tsx
+++ samples/simple-group-call/src/App.tsx
@@ -11,10 +11,6 @@ function App() {
 
     const authToken = searchParams.get('authToken');
 
-    // pass an empty string when using v2 meetings
-    // for v1 meetings, you would need to pass the correct roomName here
-    const roomName = searchParams.get('roomName') || '';
-
     if (!authToken) {
       alert(
         "An authToken wasn't passed, please pass an authToken in the URL query to join a meeting."
@@ -24,7 +20,6 @@ function App() {
 
     initMeeting({
       authToken,
-      roomName,
       defaults: {
         audio: false,
         video: false,
