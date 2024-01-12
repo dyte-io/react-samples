2eddfbc9a1faef7bef927650f2589349ccd024f4
madhugb <madhugb04@gmail.com>
2024-01-12 11:37:49 +0000

    Deploying to gh-pages from @ dyte-io/react-samples@0e1ae3d885a11a79cae95735d46c9d0c18dd8390 🚀

--- active-speaker-ui/src/components/MeetingContext.tsx
+++ active-speaker-ui/src/components/MeetingContext.tsx
@@ -1,15 +1,16 @@
 import React, { createContext, useReducer } from 'react';
 import { useDyteMeeting, useDyteSelector } from '@dytesdk/react-web-core';
-import { useThrottle } from '@uidotdev/usehooks';
+// import { useThrottle } from '@uidotdev/usehooks';
 import type { States } from '@dytesdk/ui-kit/dist/types';
 import type { Peer, Size } from '@dytesdk/ui-kit';
 import type { leaveRoomState, DytePlugin } from '@dytesdk/web-core';
 import useBreakpoint from '../utils/breakpoint';
 
-const ACTIVE_SPEAKER_CHANGE_DELAY = 3000;
+// const ACTIVE_SPEAKER_CHANGE_DELAY = 3000;
 type RoomState = 'init' | 'joined' | 'waitlisted' | leaveRoomState;
 export interface ContextValue {
 	meeting: ReturnType<typeof useDyteMeeting>['meeting'];
+	isHost: boolean;
 	states: States;
 	breakpoint: Size;
 	roomState: RoomState;
@@ -37,6 +38,7 @@ const MeetingProvider = ({ children }: Props) => {
 		}),
 		{ meeting: 'joined' },
 	);
+
 	const roomState = useDyteSelector((meeting) => meeting.self.roomState as RoomState);
 	const activeScreenshares = useDyteSelector(
 		(meeting) =>
@@ -52,6 +54,7 @@ const MeetingProvider = ({ children }: Props) => {
 	const showSpotlight = activeScreenshares.length > 0 || activePlugins.length > 0;
 
 	const lastActiveSpeaker = useDyteSelector((meeting) => meeting.participants.lastActiveSpeaker);
+
 	const isOnStage = useDyteSelector((meeting) => meeting.self.stageStatus === 'ON_STAGE');
 	const requestCount = useDyteSelector(
 		(meeting) =>
@@ -59,25 +62,19 @@ const MeetingProvider = ({ children }: Props) => {
 			meeting.participants.waitlisted.size,
 	);
 
-    const activeSpeakerInternal = meeting.participants.active
-		.toArray()
-		.find((participant) =>
-			participant.id === lastActiveSpeaker
-		) ?? meeting.self;
-	const activeSpeaker = useThrottle(activeSpeakerInternal, ACTIVE_SPEAKER_CHANGE_DELAY);
-
     return (
 		<MeetingContext.Provider
 			value={{
 				meeting,
 				states,
+				isHost: meeting.self.presetName === 'webinar_presenter',
 				updateStates,
 				roomState,
 				isScreenShareEnabled: activeScreenshares.length > 0,
 				isPluginsEnabled: activePlugins.length > 0,
 				activeScreenshares,
 				activePlugins,
-				activeSpeaker,
+				activeSpeaker: (meeting.participants.active.get(lastActiveSpeaker) ?? meeting.self) as Peer,
 				breakpoint,
 				showSpotlight,
 				requestCount,
